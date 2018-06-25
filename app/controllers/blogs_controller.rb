e8c1ca479e492619166838d2efab994e00003caf
sudhanshu7394 <sudhanshu.yadav31@gmail.com>
2018-06-25 10:03:21 +0530

    custom action via button click

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -61,6 +61,17 @@ class BlogsController < ApplicationController
     end
   end
 
+  def  toggle_status
+  if @blog.draft?
+    @blog.published!
+    elsif @blog.published?
+    @blog.draft!
+
+  end
+  redirect_to blogs_url, notice: 'Post status has been updated'
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
